d486119f37c2d18145b88193038f23a3b2679d09
pv2007 <48891543+pv2007@users.noreply.github.com>
2019-04-19 13:27:07 +0300

    Изменение настроек отступа

--- SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
@@ -1,8 +1,8 @@
 package ru.pvg.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Hello, World!!!");
-		System.out.println("Hello, World!!!");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello, World!!!");
+    System.out.println("Hello, World!!!");
+  }
 }
