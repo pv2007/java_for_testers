f48ab027bc7dd6baa34f8c0b0b06c75805fc815f
pv2007 <48891543+pv2007@users.noreply.github.com>
2019-04-19 13:52:44 +0300

    Арифметические операции

--- SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
@@ -4,5 +4,14 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     System.out.println("Hello, World!!!");
     System.out.println("Hello, World!!!");
+    System.out.println(2+2);
+    System.out.println(2*2);
+    System.out.println(2/2);
+    System.out.println(2-2);
+    System.out.println(1.0/2);
+    System.out.println(2.0/2);
+    System.out.println("2"+"3");
+    System.out.println("2"+3);
+    System.out.println("2+2="+(3+4));
   }
 }
