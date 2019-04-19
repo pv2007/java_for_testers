4c04e3980d2d02dbd5fd6e8ac59dc261cd6f5711
pv2007 <48891543+pv2007@users.noreply.github.com>
2019-04-19 13:14:15 +0300

    Создан пакет

--- SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
@@ -1,7 +1,8 @@
+package ru.pvg.sandbox;
+
 public class MyFirstProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!!!");
 		System.out.println("Hello, World!!!");
-
 	}
 }
