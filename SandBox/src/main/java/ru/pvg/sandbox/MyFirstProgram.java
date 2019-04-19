9eac1c4038d34959cbe198f512d5cb7723be1691
pv2007 <48891543+pv2007@users.noreply.github.com>
2019-04-19 14:05:00 +0300

    Откат изменений

--- SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
@@ -1,17 +1,8 @@
 package ru.pvg.sandbox;
 
 public class MyFirstProgram {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!!!");
-    System.out.println("Hello, World!!!");
-    System.out.println(2+2);
-    System.out.println(2*2);
-    System.out.println(2/2);
-    System.out.println(2-2);
-    System.out.println(1.0/2);
-    System.out.println(2.0/2);
-    System.out.println("2"+"3");
-    System.out.println("2"+3);
-    System.out.println("2+2="+(3+4));
-  }
+	public static void main(String[] args) {
+		System.out.println("Hello, World!!!");
+		System.out.println("Hello, World!!!");
+	}
 }
