70392b931a697058373fa6cf781fec5a326fcef7
pv2007 <48891543+pv2007@users.noreply.github.com>
2019-04-19 20:43:20 +0300

    Revert "Revert "Откат изменений""
    This reverts commit b0f5f3a6454b77a3e59785fe57c3b6ed5c4b0500.

--- SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
@@ -2,7 +2,11 @@ package ru.pvg.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
+		System.out.println("Hello, "+"World!!!");
 		System.out.println("Hello, World!!!");
-		System.out.println("Hello, World!!!");
+
+		int l=5;
+		int s=l*l;
+    System.out.println("Площадь квадрата со стороной " + l +" = "+s);
 	}
 }
