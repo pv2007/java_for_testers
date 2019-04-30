80d9bdc0608766d9bb708412e0ba09f130157e68
pv2007 <48891543+pv2007@users.noreply.github.com>
2019-04-30 16:28:47 +0300

    Изменение кодировки 2
    Изменение кодировки 2

--- SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
@@ -1,12 +1,12 @@
 package ru.pvg.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Привет, "+"МИР!!");
-		System.out.println("Привет, Мир!!!");
+  public static void main(String[] args) {
+    System.out.println("Привет, " + "МИР!!");
+    System.out.println("Привет, Мир!!!");
 
-		int l=5;
-		int s=l*l;
-    System.out.println("Площадь квадрата со стороной " + l +" = "+s);
-	}
+    int l = 5;
+    int s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+  }
 }
