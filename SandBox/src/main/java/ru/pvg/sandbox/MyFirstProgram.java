b4f7d8e7dfacdb55189286be354425910971d5d6
pv2007 <48891543+pv2007@users.noreply.github.com>
2019-04-30 15:43:36 +0300

    Update MyFirstProgram.java
    Изменение текста Анг - Рус

--- SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
@@ -2,8 +2,8 @@ package ru.pvg.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, "+"World!!!");
-		System.out.println("Hello, World!!!");
+		System.out.println("Привет, "+"МИР!!");
+		System.out.println("Привет, Мир!!!");
 
 		int l=5;
 		int s=l*l;
