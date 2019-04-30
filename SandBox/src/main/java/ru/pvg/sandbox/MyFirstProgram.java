cb51db6552bc7dbdb68e3333e13d2b6dcccc0da0
pv2007 <48891543+pv2007@users.noreply.github.com>
2019-04-30 17:46:08 +0300

    Добавились несколько функций

--- SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
@@ -2,11 +2,33 @@ package ru.pvg.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Привет, " + "МИР!!");
-    System.out.println("Привет, Мир!!!");
+    String someBody = "Привет, чижик!";
+    System.out.println(someBody);
+    hello("Ванюша");
+    hello("Костя");
 
-    int l = 5;
-    int s = l * l;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+    double len = 6.9;
+        System.out.println("Площадь квадрата со стороной " + len + " м = " + sQuard(len) + " кв.м.");
+
+    double len1 = 6.9;
+    double len2 = 8.9;
+    System.out.println("Площадь прямоугольника со сторонами " + len1 + " и "+ len2 +" м = " + sQuard(len1, len2) + " кв.м.");
+
+
+    double s2=sQuard(3.67);
+    System.out.println(sQuard(len));
   }
+
+  public static void hello(String nameGuest) {
+    System.out.println("ПРиветствуем " + nameGuest + "!!!");
+  }
+
+  public static double sQuard(double l) {
+    return l*l;
+  }
+
+  public static double sQuard(double a, double b) {
+    return a*b;
+  }
+
 }
