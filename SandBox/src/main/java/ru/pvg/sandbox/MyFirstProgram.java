c41bf54a90288d0728dec3a3867383b7b1ab8102
pv2007 <48891543+pv2007@users.noreply.github.com>
2019-05-01 12:14:00 +0300

    Конструкторы с this

--- SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/pvg/sandbox/MyFirstProgram.java
@@ -11,8 +11,8 @@ public class MyFirstProgram {
         System.out.println("Площадь квадрата со стороной " + len + " м = " + sQuard(len) + " кв.м.");
 */
 
-    Rectangle r = new Rectangle(6.9,8.9);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и "+ r.b +" м = " + sRect(r) + " кв.м.");
+    Rectangle r = new Rectangle(6.9, 8.9);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " м = " + sRect(r) + " кв.м.");
 
     Square s = new Square(5);
     System.out.println("Площадь квадрата со стороной " + s.l + " м = " + sQuard(s) + " кв.м.");
@@ -24,11 +24,11 @@ public class MyFirstProgram {
   }
 
   public static double sQuard(Square s) {
-    return s.l*s.l;
+    return s.l * s.l;
   }
 
   public static double sRect(Rectangle r) {
-    return r.a*r.b;
+    return r.a * r.b;
   }
 
 }
--- SandBox/src/main/java/ru/pvg/sandbox/Rectangle.java
+++ SandBox/src/main/java/ru/pvg/sandbox/Rectangle.java
@@ -4,8 +4,8 @@ public class Rectangle {
   public double a;
   public double b;
 
-  public Rectangle(double h, double w) {
-    this.a = h;
-    this.b = w;
+  public Rectangle(double a, double b) {
+    this.a = a;
+    this.b = b;
   }
 }
