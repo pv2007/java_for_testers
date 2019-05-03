84c146a633af13112ce7d79131058ddaa0dbcc2b
pv2007 <48891543+pv2007@users.noreply.github.com>
2019-05-03 10:34:44 +0300

    Использование Объектов в классах и ассоциированных Методов

--- SandBox/src/main/java/ru/pvg/sandbox/Points.java
+++ SandBox/src/main/java/ru/pvg/sandbox/Points.java
@@ -11,16 +11,8 @@ public class Points {
 
 
     public Points(Point p1, Point p2) {
-
-      if (p7 != null) {
-        this.p7.x = p1.x;
-        this.p7.y = p1.y;
-      }
-      if (p8 != null) {
-        this.p8.x = p2.x;
-        this.p8.y = p2.y;
-      }
-
+      this.p7 = p1;
+      this.p8 = p2;
     }
 
     public double distance(Point p7, Point p8) {
