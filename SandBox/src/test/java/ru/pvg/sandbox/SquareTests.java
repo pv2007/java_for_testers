2c7e9b95c94766ba6083bbd98a39da2002bf2d61
pv2007 <48891543+pv2007@users.noreply.github.com>
2019-05-03 15:13:03 +0300

    Финал MyFirstProgram + Добавлен Addressbook

--- SandBox/src/test/java/ru/pvg/sandbox/SquareTests.java
+++ SandBox/src/test/java/ru/pvg/sandbox/SquareTests.java
@@ -1,5 +1,6 @@
 package ru.pvg.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /*
@@ -10,7 +11,6 @@ public class SquareTests {
   @Test
   public void testArea(){
     Square s=new Square(5);
-    assert s.area()== 25;
-
+    Assert.assertEquals( s.area(),25.0);
   }
 }
