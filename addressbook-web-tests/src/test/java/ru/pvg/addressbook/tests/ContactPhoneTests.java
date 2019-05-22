bb261c56c8b5e0b7b70bce2022b0fca866d90036
pv2007 <48891543+pv2007@users.noreply.github.com>
2019-05-22 16:03:42 +0300

    (5/57) Резка строк. Преобразование через Регулярные выражения

--- addressbook-web-tests/src/test/java/ru/pvg/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/pvg/addressbook/tests/ContactPhoneTests.java
@@ -1,21 +1,32 @@
 package ru.pvg.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.annotations.Test;
 import ru.pvg.addressbook.model.ContactData;
 import ru.pvg.addressbook.model.Contacts;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 /*
    Created Владимир  at 11:29  20.05.2019
 */
 public class ContactPhoneTests extends TestBase {
 
-  @Test (enabled = false)
+  @Test (enabled = true)
   public void testContactPhones() {
     app.goTo().gotoPage("home");
     ContactData contact = app.contact().all().iterator().next(); //выбирается  произвольный элемент множества
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
+    assertThat(contact.getHomePhone(), equalTo(cleaned(contactInfoFromEditForm.getHomePhone())));
+    assertThat(contact.getMobilePhone(), equalTo(cleaned(contactInfoFromEditForm.getMobilePhone())));
+    assertThat(contact.getWorkPhone(), equalTo(cleaned(contactInfoFromEditForm.getWorkPhone())));
 
   }
-
+  // очистка номера от посторонних символов пробел, - ( )
+  public String cleaned(String phone) {
+    return phone.replaceAll("\\s", "").replaceAll("[-()]","");
+  }
 }
